6e545573c6efcb03ba147c1541ff32c2974fde41
[Aniket Bhosale] <[adbhosale2707]>
2024-03-03 17:24:01 +0530

    App.js file added

---
+++ Day1/App.js
@@ -0,0 +1,40 @@
+
+//This is the core of React 
+
+
+
+// const heading = React.createElement(
+//   "h2",
+//   { id: "heading", xyz: "abc" },
+//   "Hello World React!"
+// );
+
+
+
+
+/*
+what if the structure of html document is
+
+<div id="parent">  
+    <div id="child">
+        <h1>Hi</h1>
+        <h1>iam Aniket</h1>
+    </div>
+</div>
+*/
+
+// ReactElement(Object) => HTML(Browser understands)
+
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "hi"),
+    React.createElement("h1", {}, "iam aniket"),
+  ])
+);
+console.log(heading); // return an OBJECT
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
