ff765ca923e27510f303c9af0e451ac61ded0bca
[Aniket Bhosale] <[adbhosale2707]>
2024-03-08 14:39:51 +0530

    changes

--- Day1/App.js
+++ Day1/App.js
@@ -3,6 +3,43 @@ import ReactDOM from 'react-dom/client';
 
 
 
+const heading = React.createElement(
+    "h2",
+    { id: "heading", xyz: "abc" },
+    "Hello World React!"
+  );
+
+  console.log(heading);//we gets output as object
+
+  //Jsx
+
+  const jsxHeading=<h1>Hello world</h1>
+  console.log(jsxHeading);//we get same output as heading
+
+  const root=ReactDOM.createRoot(document.getElementById("root"));
+  root.render(heading);
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //This is the core of React 
 
 
@@ -29,16 +66,20 @@ what if the structure of html document is
 
 // ReactElement(Object) => HTML(Browser understands)
 
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "hi amit"),
-    React.createElement("h1", {}, "iam aniket"),
-  ])
-);
-console.log(heading); // return an OBJECT
+// const heading = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "hi amit"),
+//     React.createElement("h1", {}, "iam aniket"),
+//   ])
+// );
+// console.log(heading); // return an OBJECT
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
